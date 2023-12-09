3f154833d5578e065dde5477890edb79dc59756c
t-kusakabe <ttmac0704@gmail.com>
2023-12-09 17:21:34 +0900

    multi page対応

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: 'src',
   plugins: [react()],
+  publicDir: resolve(__dirname, 'public'),
   server: {
     host: true
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/lp/main/index.html'),
+        sub: resolve(__dirname, 'src/lp/sub/index.html')
+      }
+    }
   }
 })
