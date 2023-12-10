104004d2a7a2dbd4a8c47e6252b3b2dfd9f97ca5
t-kusakabe <ttmac0704@gmail.com>
2023-12-10 18:00:27 +0900

    Create sub main.tsx

---
+++ src/lp/sub/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <section>
+      <div>Sub Page.</div>
+    </section>
+  </React.StrictMode>,
+)
